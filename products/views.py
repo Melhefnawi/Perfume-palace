a308c870e858a7d9d3f7c7de39c1925a6551432d
Melhefnawi <>
2023-12-21 08:28:59 +0000

    updating the searching to be by categories

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product
+from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
 
@@ -9,8 +9,16 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
+
+        if 'category'in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name_in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +31,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term' : query,
+        'current_categories' : categories,
     }
 
  
