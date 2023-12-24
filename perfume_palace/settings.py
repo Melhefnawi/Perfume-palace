fba57fe35791ab1778e35b59322ee92d20661933
Melhefnawi <>
2023-12-24 12:43:07 +0000

    creating the bag app and adding the contexts contents

--- perfume_palace/settings.py
+++ perfume_palace/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'perfume_palace',
     'home',
     'products',
+    'bag',
 ]
 
 SITE_ID = 1
@@ -102,6 +103,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -185,3 +187,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
