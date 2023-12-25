e315684cb97524bd21a2679f3ba8914e49c93d7e
Melhefnawi <>
2023-12-25 14:10:48 +0000

    adjusting the bag template

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -23,3 +23,33 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag   
     
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ add quantity of a specific product """
+
+    quantity = int(request.POST.get('quantity'))
+
+    bag = request.session.get('bag',{})
+
+    if item_id in list(bag.keys()):
+        bag[item_id]+= quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag   
+    
+    return redirect(reverse('view_bag'))   
+
+def remove_from_bag(request, item_id):
+    """ add quantity of a specific product """
+    try:
+        
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)  
