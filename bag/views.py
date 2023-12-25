6b9a04b94c6ea58c6d6433e7738f1b8038fa5c8c
Melhefnawi <>
2023-12-25 15:24:07 +0000

    adjusting the adding to bag view

--- bag/views.py
+++ bag/views.py
@@ -32,10 +32,11 @@ def adjust_bag(request, item_id):
 
     bag = request.session.get('bag',{})
 
-    if item_id in list(bag.keys()):
-        bag[item_id]+= quantity
+    if quantity > 0:
+            bag[item_id] = quantity
     else:
-        bag.pop(item_id)
+            bag.pop(item_id)
+
 
     request.session['bag'] = bag   
     
