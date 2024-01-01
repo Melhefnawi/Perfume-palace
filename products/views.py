17f98a4240041a358da7338786160b7da2f2173c
Melhefnawi <>
2024-01-01 12:12:22 +0000

    update the product view to add products

--- products/views.py
+++ products/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.db.models.functions import Lower
 from django.db.models import Q
 
+from .forms import ProductForm
+
 # Create your views here.
 
 def all_products(request):
@@ -71,3 +73,13 @@ def product_detail(request, product_id):
 
  
     return render(request, 'products/product_detail.html', context)    
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)    
