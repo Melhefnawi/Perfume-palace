2b6a78bbe8dd02285be9e697aaf75463772cd035
Melhefnawi <>
2024-01-18 09:09:49 +0000

    removing cloudanary

--- perfume_palace/settings.py
+++ perfume_palace/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
-    'cloudinary_storage',
+    
     'django.contrib.staticfiles',
-    'cloudinary',
+    
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -202,11 +202,11 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
