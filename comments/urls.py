fe060e3235a047ca7b7c65013ba229973e485913
Melhefnawi <>
2024-01-25 09:53:49 +0000

    adjust the add_commit function

--- comments/urls.py
+++ comments/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('add_comments/', views.add_comment, name='add_comments'),
+    path('add/', views.add_comment, name='add_comment'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from comments.forms import CommentForm
+from .forms import CommentForm
 
 
 @login_required
 def add_comment(request):
     """ Add a comments to the store """
-    if not request.user.is_superuser:
+    if not request.user.is_authenticated:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
-        form = CommentForm(request.POST, request.FILES)
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save()
             messages.success(request, 'Successfully added your comment!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('product_detail'))
         else:
             messages.error(request, 'Failed to add your comment form is unvalid.')
     else:
