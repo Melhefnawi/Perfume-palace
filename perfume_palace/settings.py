a5aa6eb352233b9979c00ed4c2bf397d87a463b2
Melhefnawi elhefnawy <mmm_hefnawy@yahoo.com>
2024-03-29 15:26:35 +0000

    fixing the email confirmation issue

--- perfume_palace/settings.py
+++ perfume_palace/settings.py
@@ -29,11 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-melhefnawi-perfume-palac-v2vfz4b4t1.us2.codeanyapp.com',
-                 "localhost", 'perfumepalace-85931537055e.herokuapp.com']
+ALLOWED_HOSTS = [
+                 "localhost", 'perfumepalace-85931537055e.herokuapp.com','8000-melhefnawi-perfumepalac-y5mr22flx07.ws-eu110.gitpod.io']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
